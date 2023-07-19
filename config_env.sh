877b7fdef3790cd526f763c9a684a06e2f7ac16d
yimingolaola <du.yiming@olaola.chat>
2023-07-19 17:05:47 +0800

    add script for updating nginx config
    (cherry picked from commit 6634e9a5c90b71e2b97ff5cfa7db5e1b9b7f2dc4)

---
+++ config_env.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# refer to https://medium.com/hackernoon/truly-atomic-deployments-with-nginx-and-php-fpm-aed8a8ac1cd9
+fastcgi_param = 'fastcgi_param SCRIPT_FILENAME $realpath_root$fastcgi_script_name;'
+nginx_fastcgi_param_path = '/etc/nginx/fastcgi_params'
+local_setting = "./nginx_settings.conf"
+remote_setting = "/etc/nginx/sites-available/default"
+# only insert when the param does not exist
+if ! grep -qF "$fastcgi_param" $nginx_fastcgi_param_path; then
+    sudo sed -i "1i\\$fastcgi_param" $nginx_fastcgi_param_path
+fi
+
+# aligning the local nginx conifg to nginx_settings.conf
+if cmp -s $local_setting $remote_setting; then
+    echo "No difference found between nginx_settings.conf and sites-available default settings, skip rewritting local config."
+else
+    echo "The content of nginx_settings.conf and sites-available default setting is different. Updating local config..."
+    # back up config
+    current_date=$(date)
+    back_up_name="default_$current_date.backup"
+    sudo cp $remote_setting /etc/nginx/sites-available/$back_up_name
+    sudo cat $local_setting > $remote_setting
+
+    echo "Done updating local config. Back up file of the older config: $back_up_name"
+fi
