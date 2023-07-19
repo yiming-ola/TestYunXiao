55ec566b4ad9db25bb5ee6fd72634ee628072377
yimingolaola <du.yiming@olaola.chat>
2023-07-19 18:02:58 +0800

    .

--- deploy/config_env.sh
+++ deploy/config_env.sh
@@ -6,9 +6,12 @@ nginx_fastcgi_param_path='/etc/nginx/fastcgi_params'
 local_setting='/home/webroot/origin/deploy/nginx_settings.conf'
 remote_setting='/etc/nginx/sites-available/default'
 # only insert when the param does not exist
-if ! grep -qF "$fastcgi_param" $nginx_fastcgi_param_path; then
+touch $remote_setting
+if ! grep -qF $fastcgi_param $nginx_fastcgi_param_path; then
     echo "Inserting fastcgi_param..."
     sudo sed -i "1i\\$fastcgi_param" $nginx_fastcgi_param_path
+else
+    echo "fastcgi_param ok."
 fi
 
 # aligning the local nginx conifg to nginx_settings.conf
