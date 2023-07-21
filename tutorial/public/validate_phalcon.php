a4b36d90f5190957eb193713d09b43a34645baba
yimingolaola <du.yiming@olaola.chat>
2023-07-21 17:43:28 +0800

    .

--- tutorial/public/validate_phalcon.php
+++ tutorial/public/validate_phalcon.php
@@ -1,10 +1,10 @@
 <?php
 define('BASE_PATH', dirname(__DIR__));
-echo (BASE_PATH);
+echo ("base path: " . BASE_PATH);
 define('APP_PATH', BASE_PATH . '/app');
 require_once BASE_PATH . '/vendor/autoload.php';
 if (!extension_loaded('phalcon')) {
     die("Phalcon extension is not loaded. Please check if the extension is properly installed.");
 }
 // require_once 'vendor/autoload.php'; // Include the Composer autoloader
-// echo "Phalcon Version: " . Phalcon\Version::get();
+echo "Phalcon Version: " . Phalcon\Version::get();
