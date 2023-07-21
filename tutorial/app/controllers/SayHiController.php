d1a9c31382e1098d5f76acb7b357b573950e0767
yimingolaola <du.yiming@olaola.chat>
2023-07-21 17:54:11 +0800

    .

--- tutorial/app/controllers/SayHiController.php
+++ tutorial/app/controllers/SayHiController.php
@@ -4,6 +4,9 @@ use Phalcon\Mvc\Controller;
 
 class SayHiController extends Controller
 {
+    public function indexAction()
+    {
+    }
     public function SayHiAction()
     {
         return '<h1>Hi YM Good</h1>';
