b24fa5f82b1db2d832d59bc165835ba95b010413
yimingolaola <du.yiming@olaola.chat>
2023-07-25 11:35:18 +0800

    Not changing root nginx config
    .
    
    .

--- deploy/config_env.sh
+++ deploy/config_env.sh
@@ -2,29 +2,17 @@
 
 # switch user to ecs-user
 
-# refer to https://medium.com/hackernoon/truly-atomic-deployments-with-nginx-and-php-fpm-aed8a8ac1cd9
-fastcgi_param='fastcgi_param SCRIPT_FILENAME $realpath_root$fastcgi_script_name;'
+local_setting='/home/ecs-user/webroot/deploy/nginx_settings.conf'
+remote_setting='/home/ecs-user/.local/etc/nginx/sites-available/default'
+enabled_setting='/home/ecs-user/.local/etc/nginx/sites-enabled/'
+back_up_dir='/home/ecs-user/.local/etc/nginx/sites-available/back_up'
 
-nginx_fastcgi_param_path='/etc/nginx/fastcgi_params'
-local_setting='/home/webroot/origin/deploy/nginx_settings.conf'
-remote_setting='/etc/nginx/sites-available/default'
-enabled_setting='/etc/nginx/sites-enabled/'
-back_up_dir='/etc/nginx/sites-available/back_up'
-
-# only insert when the param does not exist
 if [ ! -e $remote_setting ]; then
     touch $remote_setting
 fi
 
 # link settings
-sudo ln -s $remote_setting $enabled_setting
-
-if ! grep -qF "$fastcgi_param" $nginx_fastcgi_param_path; then
-    echo "Inserting fastcgi_param..."
-    sudo sed -i "1i\\$fastcgi_param" $nginx_fastcgi_param_path
-else
-    echo "Fastcgi_param ok."
-fi
+ln -s $remote_setting $enabled_setting
 
 # aligning the local nginx conifg to nginx_settings.conf
 if cmp -s $local_setting $remote_setting; then
@@ -39,26 +27,28 @@ else
     back_up_file="$back_up_dir/$back_up_name"
 
     # back up config
-    sudo cp $remote_setting $back_up_file
+    cp $remote_setting $back_up_file
 
     # replace nginx config
-    sudo cat $local_setting > $remote_setting
+    cat $local_setting > $remote_setting
 
     # check config file
-    sudo nginx -t
+    nginx -t
     if [ $? -eq 0 ]; then
         echo "Nginx configuration test passed successfully. Updating and reloading Nginx..."
         # reload nginx
-        sudo nginx -s reload
+        nginx -s reload
         echo "Done updating local config. Back up file of the older config: $back_up_name"
     else
         echo "Nginx configuration test failed!!!! restore changes to the config."
 
         # restore config file
-        sudo cp $back_up_file $remote_setting
-        sudo rm -rf $back_up_file
+        cp $back_up_file $remote_setting
+        rm -rf $back_up_file
     fi
 fi
 
 # deleting back up files older than 14 days.
 find $back_up_dir -type f -mtime +14 -exec rm {} \;
+
+# link to application path
