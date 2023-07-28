cd638e03b5ebed4b1d32e97bdebf4189e10ee827
yimingolaola <du.yiming@olaola.chat>
2023-07-28 18:46:58 +0800

    .

--- deploy/targeting.sh
+++ deploy/targeting.sh
@@ -6,22 +6,28 @@ script_path="$0"
 # Extract the directory path from the script path
 script_directory=$(dirname "$script_path")
 
-task_name="$1"
+task_file_name="$1.conf"
 
 # loop through config files to locate where the task is
 configs_dir="$script_directory/supervisor"
+
+target_cmd=""
 
 function process_files_recursively() {
     local current_dir="$1"
 
     for file in "$current_dir"/*; do
         if [ -f "$file" ]; then
-            # Perform your desired actions with the file here
-            echo "Processing file: $file"
-            # Add more commands as needed, for example:
-            # do_something_with_file "$file"
+            file_name=$(basename "$file")
+
+            target_file
+
+            if [[ "$file_name" == "$task_file_name" ]]; then
+                dir_name=$(dirname "$file")
+                target_cmd=$(basename "$dir_name")
+                echo "Processing matching file: $file_name, target: $target_cmd"
+            fi
         elif [ -d "$file" ]; then
-            # If the item is a subdirectory, call the function recursively
             process_files_recursively "$file"
         fi
     done
